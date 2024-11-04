10b040556e2d2fcb18e55d07b1c787c91eebed71
rafatocinoo <rtocbat936@g.educaand.es>
2024-11-04 12:30:10 +0100

    Update BASTIONADO.js

--- BASTIONADO/BASTIONADO.js
+++ BASTIONADO/BASTIONADO.js
@@ -3,5 +3,14 @@ document.querySelectorAll('.project').forEach(item => {
         const url = item.getAttribute('data-url');
         window.location.href = url;  // Redirige a la URL especificada
     });
-});
+    
+    item.addEventListener('mouseenter', () => {
+        const description = item.querySelector('.description');
+        description.style.display = 'block'; // Muestra la descripción al pasar el mouse
+    });
 
+    item.addEventListener('mouseleave', () => {
+        const description = item.querySelector('.description');
+        description.style.display = 'none'; // Oculta la descripción al salir el mouse
+    });
+});
